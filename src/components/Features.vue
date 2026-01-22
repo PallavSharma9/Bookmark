2b3ff5585efc8018f540a1cbe40b2d7fda2c3e40
PallavSharma9 <pallavsharma0202@gmail.com>
2026-01-22 19:29:58 +0530

    bookmark page

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -62,6 +62,7 @@ const panel = ref("simple-bookmarking");
 
     <!-- panel selection section  -->
     <div
+      v-if="panel === 'simple-bookmarking'"
       class="mt-12 flex flex-col justify-center items-center space-y-18 lg:space-y-0 lg:space-x-8 lg:flex-row lg:items-start lg:justify-start"
     >
       <div class="lg:w-1/2">
@@ -80,6 +81,62 @@ const panel = ref("simple-bookmarking");
           Organize your bookmarks however you like. Our simple drag-and-drop
           interface gives you complete control over how you manage your
           favourite sites.
+        </p>
+        <RouterLink
+          to="/"
+          class="px-6 py-2 rounded-lg shadow-lg border-2 border-blue-600 bg-blue-600 text-white font-bold hover:bg-white hover:text-blue-600 lg:mt-8"
+          >More Info</RouterLink
+        >
+      </div>
+    </div>
+
+    <div
+      v-if="panel === 'speedy-searching'"
+      class="mt-12 flex flex-col justify-center items-center space-y-18 lg:space-y-0 lg:space-x-8 lg:flex-row lg:items-start lg:justify-start"
+    >
+      <div class="lg:w-1/2">
+        <img
+          src="../../public/images/illustration-features-tab-2.svg"
+          class="mx-auto lg:mr-0"
+          alt=""
+        />
+      </div>
+
+      <div
+        class="flex flex-col justify-center items-center space-y-8 lg:w-1/2 lg:items-start lg:justify-start"
+      >
+        <h1 class="text-3xl lg:text-4xl font-bold">Intelligent search</h1>
+        <p class="text-gray-500 w-sm">
+          Our powerful search feature will help you find saved sites in no time
+          at all. No need to trawl through all of your bookmarks.
+        </p>
+        <RouterLink
+          to="/"
+          class="px-6 py-2 rounded-lg shadow-lg border-2 border-blue-600 bg-blue-600 text-white font-bold hover:bg-white hover:text-blue-600 lg:mt-8"
+          >More Info</RouterLink
+        >
+      </div>
+    </div>
+
+    <div
+      v-if="panel === 'easy-sharing'"
+      class="mt-12 flex flex-col justify-center items-center space-y-18 lg:space-y-0 lg:space-x-8 lg:flex-row lg:items-start lg:justify-start"
+    >
+      <div class="lg:w-1/2">
+        <img
+          src="../../public/images/illustration-features-tab-3.svg"
+          class="mx-auto lg:mr-0"
+          alt=""
+        />
+      </div>
+
+      <div
+        class="flex flex-col justify-center items-center space-y-8 lg:w-1/2 lg:items-start lg:justify-start"
+      >
+        <h1 class="text-3xl lg:text-4xl font-bold">Share your bookmarks</h1>
+        <p class="text-gray-500 w-sm">
+          Easily share your bookmarks and collections with others. Create a
+          shareable a link that you can send at the click of a button.
         </p>
         <RouterLink
           to="/"
